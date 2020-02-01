67562911252c2eb912f8c62959492035d00452a3
hungnguyen21B <54875837+hungnguyen21B@users.noreply.github.com>
2020-02-01 10:29:10 +0700

    Add files via upload

--- Uku/index.php
+++ Uku/index.php
@@ -14,7 +14,6 @@ require 'Controller/productController.php';
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <span class="top-contact">NEED ASSISTANCE? 80324609687</span>
